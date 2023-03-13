3debce0002d904e425d2c80d5b31c559ef4c9b2d
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2023-03-13 11:54:03 +0100

    Added active scene logic

--- Engine/SceneManager.cpp
+++ Engine/SceneManager.cpp
@@ -1,6 +1,12 @@
 #include "SceneManager.h"
 
 
+SceneManager::~SceneManager()
+{
+	for (const auto& scene : m_Scenes)
+		delete scene;
+}
+
 void SceneManager::Init() const
 {
 	for (const auto& scene : m_Scenes)
@@ -9,24 +15,37 @@ void SceneManager::Init() const
 
 void SceneManager::FixedUpdate() const
 {
-	for (const auto& scene : m_Scenes)
-		scene->FixedUpdate();
+	if (!m_Scenes.empty())
+		m_Scenes[m_ActiveSceneIndex]->FixedUpdate();
 }
 
 void SceneManager::Update() const
 {
-	for (const auto& scene : m_Scenes)
-		scene->Update();
+	if (!m_Scenes.empty())
+		m_Scenes[m_ActiveSceneIndex]->Update();
 }
 
 void SceneManager::LateUpdate() const
 {
-	for (const auto& scene : m_Scenes)
-		scene->LateUpdate();
+	if (!m_Scenes.empty())
+		m_Scenes[m_ActiveSceneIndex]->LateUpdate();
 }
 
 void SceneManager::Render() const
 {
-	for (const auto& scene : m_Scenes)
-		scene->Render();
+	if (!m_Scenes.empty())
+		m_Scenes[m_ActiveSceneIndex]->Render();
+}
+
+uint32_t SceneManager::AddScene(GameScene* scene)
+{
+	m_Scenes.emplace_back(scene);
+	return uint32_t(m_Scenes.size() - 1);
+}
+
+void SceneManager::SetActiveScene(uint32_t sceneIndex)
+{
+	//Prevent setting scene index to a non existing scene
+	if (sceneIndex < m_Scenes.size())
+		m_ActiveSceneIndex = sceneIndex;
 }
--- Engine/SceneManager.h
+++ Engine/SceneManager.h
@@ -5,10 +5,10 @@
 #include "GameScene.h"
 #include "Singleton.h"
 
-class SceneManager final: public Singleton<SceneManager>
+class SceneManager final : public Singleton<SceneManager>
 {
 public:
-	~SceneManager() override = default;
+	~SceneManager() override;
 
 	SceneManager(const SceneManager& other) noexcept = delete;
 	SceneManager& operator=(const SceneManager& other) noexcept = delete;
@@ -21,11 +21,14 @@ public:
 	void LateUpdate() const;
 	void Render() const;
 
+	uint32_t AddScene(GameScene* scene);
+	void SetActiveScene(uint32_t sceneIndex);
+
 private:
 	friend class Singleton<SceneManager>;
 	SceneManager() noexcept = default;
 
-	std::vector<std::unique_ptr<GameScene>> m_Scenes;
-	
-};
+	std::vector<GameScene*> m_Scenes;
+	uint32_t m_ActiveSceneIndex{};
 
+};
