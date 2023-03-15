4fa4a3a6b4c08e596ba463aa6a91b182e6fb0c7f
Arnaud Vanderveken <vanderveken.arnaud@gmail.com>
2023-03-15 15:07:36 +0100

    Implemented transform class with dirty flag

--- Engine/Transform.cpp
+++ Engine/Transform.cpp
@@ -1,1 +1,108 @@
 #include "Transform.h"
+
+using namespace DirectX;
+
+const XMFLOAT3& Transform::GetPosition() const
+{
+	return m_Position;
+}
+
+const XMFLOAT4& Transform::GetRotation() const
+{
+	return m_Rotation;
+}
+
+const XMFLOAT3& Transform::GetScale() const
+{
+	return m_Scale;
+}
+
+const XMFLOAT4X4& Transform::GetTransform()
+{
+	if (m_DirtyTransform)
+		RebuildTransform();
+
+	return m_Transform;
+}
+
+void Transform::SetPosition(float x, float y, float z)
+{
+	m_Position.x = x;
+	m_Position.y = y;
+	m_Position.z = z;
+
+	m_DirtyTransform = true;
+}
+
+void Transform::SetPosition(const XMFLOAT3& position)
+{
+	m_Position = position;
+
+	m_DirtyTransform = true;
+}
+
+void Transform::SetPosition(const XMVECTOR& position)
+{
+	XMStoreFloat3(&m_Position, position);
+
+	m_DirtyTransform = true;
+}
+
+void Transform::SetRotation(float x, float y, float z, bool isDegree)
+{
+	if (isDegree)
+		XMStoreFloat4(&m_Rotation, XMQuaternionRotationRollPitchYaw(XMConvertToRadians(x), XMConvertToRadians(y), XMConvertToRadians(z)));
+	
+	else
+		XMStoreFloat4(&m_Rotation, XMQuaternionRotationRollPitchYaw(x, y, z));
+
+	m_DirtyTransform = true;	
+}
+
+void Transform::SetRotation(const XMFLOAT3& rotation, bool isDegree)
+{
+	SetRotation(rotation.x, rotation.y, rotation.z, isDegree);
+}
+
+void Transform::SetRotation(const XMFLOAT4& rotation)
+{
+	m_Rotation = rotation;
+
+	m_DirtyTransform = true;
+}
+
+void Transform::SetRotation(const XMVECTOR& rotation)
+{
+	XMStoreFloat4(&m_Rotation, rotation);
+
+	m_DirtyTransform = true;
+}
+
+void Transform::SetScale(float x, float y, float z)
+{
+	m_Scale.x = x;
+	m_Scale.y = y;
+	m_Scale.z = z;
+
+	m_DirtyTransform = true;
+}
+
+void Transform::SetScale(const XMFLOAT3& scale)
+{
+	m_Scale = scale;
+
+	m_DirtyTransform = true;
+}
+
+void Transform::SetScale(float s)
+{
+	SetScale(s, s, s);
+}
+
+void Transform::RebuildTransform()
+{
+	const auto world = XMMatrixScaling(m_Scale.x, m_Scale.y, m_Scale.z) * XMMatrixRotationQuaternion(XMLoadFloat4(&m_Rotation)) * XMMatrixTranslation(m_Position.x, m_Position.y, m_Position.z);
+	XMStoreFloat4x4(&m_Transform, world);
+
+	m_DirtyTransform = false;
+}
--- Engine/Transform.h
+++ Engine/Transform.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <DirectXMath.h>
+
 
 class Transform final
 {
@@ -12,9 +14,98 @@ public:
 	Transform(Transform&& other) noexcept = delete;
 	Transform& operator=(Transform&& other) noexcept = delete;
 
+	/**
+	 * \brief 
+	 * \return Position vector
+	 */
+	[[nodiscard]] const DirectX::XMFLOAT3& GetPosition() const;
+	/**
+	 * \brief 
+	 * \return Rotation quaternion
+	 */
+	[[nodiscard]] const DirectX::XMFLOAT4& GetRotation() const;
+	/**
+	 * \brief 
+	 * \return Scale vector
+	 */
+	[[nodiscard]] const DirectX::XMFLOAT3& GetScale() const;
+	/**
+	 * \brief 
+	 * \return Transform matrix
+	 */
+	[[nodiscard]] const DirectX::XMFLOAT4X4& GetTransform();
+
+	/**
+	 * \brief Set position vector
+	 * \param x Position.x
+	 * \param y Position.y
+	 * \param z Position.z
+	 */
+	void SetPosition(float x, float y, float z);
+	/**
+	 * \brief Set position vector
+	 * \param position Position vector as XMFLOAT3
+	 */
+	void SetPosition(const DirectX::XMFLOAT3& position);
+	/**
+	 * \brief Set position vector
+	 * \param position Position vector as XMVECTOR
+	 */
+	void SetPosition(const DirectX::XMVECTOR& position);
+
+	/**
+	 * \brief Set rotation quaternion from Euler angles
+	 * \param x Angle around x-axis
+	 * \param y Angle around y-axis
+	 * \param z Angle around z-axis
+	 * \param isDegree Toggle between degrees and radians, degrees by default
+	 */
+	void SetRotation(float x, float y, float z, bool isDegree = true);
+	/**
+	 * \brief Set rotation quaternion from Euler angles
+	 * \param rotation Euler angles around x- y- and z-axis
+	 * \param isDegree Toggle between degrees and radians, degrees by default
+	 */
+	void SetRotation(const DirectX::XMFLOAT3& rotation, bool isDegree = true);
+	/**
+	 * \brief Set rotation quaternion
+	 * \param rotation Rotation quaternion as XMFLOAT4
+	 */
+	void SetRotation(const DirectX::XMFLOAT4& rotation);
+	/**
+	 * \brief Set rotation quaternion
+	 * \param rotation Rotation quaternion as XMVECTOR
+	 */
+	void SetRotation(const DirectX::XMVECTOR& rotation);
+
+	/**
+	 * \brief Set scale vector
+	 * \param x Scale.x
+	 * \param y Scale.y
+	 * \param z Scale.z
+	 */
+	void SetScale(float x, float y, float z);
+	/**
+	 * \brief Set uniform scale
+	 * \param s Scale
+	 */
+	void SetScale(float s);
+	/**
+	 * \brief Set scale vector
+	 * \param scale Scale vector as XMFLOAT3
+	 */
+	void SetScale(const DirectX::XMFLOAT3& scale);
+
 private:
 	/* DATA MEMBERS */
+	DirectX::XMFLOAT3 m_Position{};
+	DirectX::XMFLOAT4 m_Rotation{};
+	DirectX::XMFLOAT3 m_Scale{};
+
+	DirectX::XMFLOAT4X4 m_Transform{};
+
+	bool m_DirtyTransform{};
 
 	/* PRIVATE METHODS */
-
+	void RebuildTransform();
 };
